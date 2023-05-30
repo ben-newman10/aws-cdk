f67b56076904eeef816218a649d08a5dfdae3a4e
Ben N <ben.n@dev.net>
2023-05-30 21:43:00 +0100

    add changes

--- sample_app/sample_app_stack.py
+++ sample_app/sample_app_stack.py
@@ -1,26 +1,31 @@
 from constructs import Construct
 from aws_cdk import (
-    Duration,
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw
 )
 
+from .hitcounter import HitCounter
 
 class SampleAppStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "SampleAppQueue",
-            visibility_timeout=Duration.seconds(300),
+        # Start here
+        my_lambda = _lambda.Function(
+            self, 'HeloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset('lambda'),
+            handler='hello.handler',
         )
 
-        topic = sns.Topic(
-            self, "SampleAppTopic"
+        hello_with_counter = HitCounter(
+            self, 'HelloHitCounter',
+            downstream=my_lambda,
         )
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        my_apigw = apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=hello_with_counter.handler,
+        )
